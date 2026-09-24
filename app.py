842a0580234cf5299049097e05f71f5a9b3718d0
vrushali <kabadivrushali97@gmail.com>
2026-09-24 08:17:29 +0000

    Update the application

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-print("Welcome to DevOps Week 02")
+from flask import Flask
 
-print("Feature 1: Application setup completed")
+app = Flask(__name__)
 
-print("Feature 2: New application changes")
+@app.route("/")
+def home():
+    return """
+    <h1>Welcome to DevOps Week 02</h1>
+    <p>Feature 1: Application setup completed</p>
+    <p>Feature 2: New application changes</p>
+    """
+
+app.run(host="0.0.0.0", port=3000)
